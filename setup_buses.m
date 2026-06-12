43c5cf5c36994d93e86e14ca25f68641326f3aa5
lolve <lolve@Aleksei>
2026-06-12 16:49:23 +0300

    Очистил чарт. Вычисления std, mean ушли в NH_Metrics. Добавил вычисление длины окна буфера от толщины (4-8-12-16). Буфер стал 16. Добавился setup_buses.m для шины. в my_io.slx уменьшил буфер, приложение стало чуть живее отрисовыывать сигнал

---
+++ setup_buses.m
@@ -0,0 +1,103 @@
+function setup_buses() 
+% SETUP_BUSES initializes a set of bus objects in the MATLAB base workspace 
+
+% Bus object: NH_MetricsBus_Type 
+clear elems;
+elems(1) = Simulink.BusElement;
+elems(1).Name = 'Nlast';
+elems(1).Dimensions = 1;
+elems(1).DimensionsMode = 'Fixed';
+elems(1).DataType = 'double';
+elems(1).Complexity = 'real';
+elems(1).Min = [];
+elems(1).Max = [];
+elems(1).DocUnits = '';
+elems(1).Description = '';
+
+elems(2) = Simulink.BusElement;
+elems(2).Name = 'Ndif';
+elems(2).Dimensions = 1;
+elems(2).DimensionsMode = 'Fixed';
+elems(2).DataType = 'double';
+elems(2).Complexity = 'real';
+elems(2).Min = [];
+elems(2).Max = [];
+elems(2).DocUnits = '';
+elems(2).Description = '';
+
+elems(3) = Simulink.BusElement;
+elems(3).Name = 'Nstd';
+elems(3).Dimensions = 1;
+elems(3).DimensionsMode = 'Fixed';
+elems(3).DataType = 'double';
+elems(3).Complexity = 'real';
+elems(3).Min = [];
+elems(3).Max = [];
+elems(3).DocUnits = '';
+elems(3).Description = '';
+
+elems(4) = Simulink.BusElement;
+elems(4).Name = 'Nmean';
+elems(4).Dimensions = 1;
+elems(4).DimensionsMode = 'Fixed';
+elems(4).DataType = 'double';
+elems(4).Complexity = 'real';
+elems(4).Min = [];
+elems(4).Max = [];
+elems(4).DocUnits = '';
+elems(4).Description = '';
+
+elems(5) = Simulink.BusElement;
+elems(5).Name = 'Hlast';
+elems(5).Dimensions = 1;
+elems(5).DimensionsMode = 'Fixed';
+elems(5).DataType = 'double';
+elems(5).Complexity = 'real';
+elems(5).Min = [];
+elems(5).Max = [];
+elems(5).DocUnits = '';
+elems(5).Description = '';
+
+elems(6) = Simulink.BusElement;
+elems(6).Name = 'Hdif';
+elems(6).Dimensions = 1;
+elems(6).DimensionsMode = 'Fixed';
+elems(6).DataType = 'double';
+elems(6).Complexity = 'real';
+elems(6).Min = [];
+elems(6).Max = [];
+elems(6).DocUnits = '';
+elems(6).Description = '';
+
+elems(7) = Simulink.BusElement;
+elems(7).Name = 'Hstd';
+elems(7).Dimensions = 1;
+elems(7).DimensionsMode = 'Fixed';
+elems(7).DataType = 'double';
+elems(7).Complexity = 'real';
+elems(7).Min = [];
+elems(7).Max = [];
+elems(7).DocUnits = '';
+elems(7).Description = '';
+
+elems(8) = Simulink.BusElement;
+elems(8).Name = 'Hmean';
+elems(8).Dimensions = 1;
+elems(8).DimensionsMode = 'Fixed';
+elems(8).DataType = 'double';
+elems(8).Complexity = 'real';
+elems(8).Min = [];
+elems(8).Max = [];
+elems(8).DocUnits = '';
+elems(8).Description = '';
+
+NH_MetricsBus_Type = Simulink.Bus;
+NH_MetricsBus_Type.HeaderFile = '';
+NH_MetricsBus_Type.Description = '';
+NH_MetricsBus_Type.DataScope = 'Auto';
+NH_MetricsBus_Type.Alignment = -1;
+NH_MetricsBus_Type.PreserveElementDimensions = 0;
+NH_MetricsBus_Type.Elements = elems;
+clear elems;
+assignin('base','NH_MetricsBus_Type', NH_MetricsBus_Type);
+
