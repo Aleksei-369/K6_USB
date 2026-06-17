0ad7242d23daaa089f0ee1fbfadfd0d41943c1eb
lolve <lolve@Aleksei>
2026-06-17 19:26:12 +0300

    данные из приложения в модель идут через udp. Далее собираем в шину FromAppBus. Изменился класс FromApp

---
+++ FromApp.m
@@ -0,0 +1,76 @@
+classdef FromApp
+    % FROMAPP Класс конфигурации данных, передаваемых из интерфейса в Simulink
+    
+    properties
+        % Уставки (скаляры)
+        Amplitude (1,1) double = 800.0
+        Periods   (1,1) double = 2.0
+        Sharpness (1,1) double = 2.0
+        
+        % Таблицы калибровки LUT (векторы по 25 элементов)
+        xx        (1,25) double = [41343 41343.1 41343.2 41343.3 41343.4 41343.5 41343.6 41343.7 41343.8 41343.9 41344 41682 41973 42429 43533 44717 46388 47769 49334 50678 51343 53185 55663 57862 60000]
+        yy        (1,25) double = [3122 3121.9 3121.8 3121.7 3121.6 3121.5 3121.4 3121.3 3121.2 3121.1 3121 2440 1964 1465 976 698 466 335 232 181 154 103 51 20 0]
+        
+        Kzero     (1,1) double = 0.0296
+        AirGrad   (1,1) double = 41000
+        
+        % Команды (логические флаги кнопок)
+        CmdStart  (1,1) logical = false
+        CmdZero   (1,1) logical = false
+        CmdAir    (1,1) logical = false
+
+        CmdRsrv1  (1,1) logical = false
+        CmdRsrv2  (1,1) logical = false
+        CmdRsrv3  (1,1) logical = false
+        CmdRsrv4  (1,1) logical = false
+    end
+    
+    methods
+        function array = toArray(obj)
+            % Динамический сборщик: сканирует свойства класса FromApp 
+            % и собирает их в один плоский вектор типа double для UDP.
+            meta = metaclass(obj);
+            propNames = {meta.PropertyList.Name};
+            
+            array = [];
+            for i = 1:length(propNames)
+                % Забираем значение, приводим к double и вытягиваем в строку
+                val = double(obj.(propNames{i}));
+                array = [array, val(:)']; 
+            end
+        end
+    end
+    
+    methods (Static)
+        function types = getSimulinkTypes()
+            % Возвращает типы данных для портов блока Byte Unpack
+            meta = ?FromApp;
+            propCount = length(meta.PropertyList);
+            types = repmat({'double'}, 1, propCount);
+        end
+        
+        function dims = getSimulinkDimensions()
+            % Автоматически сканирует свойства класса и задает размерность 
+            % выходных портов блока Byte Unpack (скаляры или векторы по 25 элементов)
+            meta = ?FromApp;
+            props = meta.PropertyList;
+            dims = cell(1, length(props));
+            for i = 1:length(props)
+                dims{i} = props(i).Validation.Size; 
+            end
+        end
+
+        function bytes = getRequiredBufferSize()
+            % Прямой и надежный расчет без тяжелой рефлексии метаклассов.
+            % Гарантирует возвращение чистого числового значения double.
+            
+            numScalars = 5;  % Amplitude, Periods, Sharpness
+            numVectors = 2;  % xx, yy
+            vecLength  = 25; % Длина каждого вектора
+            numButtons = 7;  % CmdStart, CmdZero, CmdAir
+            
+            totalElements = numScalars + (numVectors * vecLength) + numButtons;
+            bytes = double(totalElements * 8); % Явно приводим к double
+        end
+    end
+end
--- setup_buses.m
+++ setup_buses.m
@@ -101,3 +101,169 @@ NH_MetricsBus_Type.Elements = elems;
 clear elems;
 assignin('base','NH_MetricsBus_Type', NH_MetricsBus_Type);
 
+% Bus object: FromAppBus_Type 
+clear elems;
+elems(1) = Simulink.BusElement;
+elems(1).Name = 'Amplitude';
+elems(1).Dimensions = 1;
+elems(1).DimensionsMode = 'Fixed';
+elems(1).DataType = 'double';
+elems(1).Complexity = 'real';
+elems(1).Min = [];
+elems(1).Max = [];
+elems(1).DocUnits = '';
+elems(1).Description = '';
+
+elems(2) = Simulink.BusElement;
+elems(2).Name = 'Periods';
+elems(2).Dimensions = 1;
+elems(2).DimensionsMode = 'Fixed';
+elems(2).DataType = 'double';
+elems(2).Complexity = 'real';
+elems(2).Min = [];
+elems(2).Max = [];
+elems(2).DocUnits = '';
+elems(2).Description = '';
+
+elems(3) = Simulink.BusElement;
+elems(3).Name = 'Sharpness';
+elems(3).Dimensions = 1;
+elems(3).DimensionsMode = 'Fixed';
+elems(3).DataType = 'double';
+elems(3).Complexity = 'real';
+elems(3).Min = [];
+elems(3).Max = [];
+elems(3).DocUnits = '';
+elems(3).Description = '';
+
+elems(4) = Simulink.BusElement;
+elems(4).Name = 'xx';
+elems(4).Dimensions = 1;
+elems(4).DimensionsMode = 'Fixed';
+elems(4).DataType = 'double';
+elems(4).Complexity = 'real';
+elems(4).Min = [];
+elems(4).Max = [];
+elems(4).DocUnits = '';
+elems(4).Description = '';
+
+elems(5) = Simulink.BusElement;
+elems(5).Name = 'yy';
+elems(5).Dimensions = 1;
+elems(5).DimensionsMode = 'Fixed';
+elems(5).DataType = 'double';
+elems(5).Complexity = 'real';
+elems(5).Min = [];
+elems(5).Max = [];
+elems(5).DocUnits = '';
+elems(5).Description = '';
+
+elems(6) = Simulink.BusElement;
+elems(6).Name = 'Kzero';
+elems(6).Dimensions = 1;
+elems(6).DimensionsMode = 'Fixed';
+elems(6).DataType = 'double';
+elems(6).Complexity = 'real';
+elems(6).Min = [];
+elems(6).Max = [];
+elems(6).DocUnits = '';
+elems(6).Description = '';
+
+elems(7) = Simulink.BusElement;
+elems(7).Name = 'AirGrad';
+elems(7).Dimensions = 1;
+elems(7).DimensionsMode = 'Fixed';
+elems(7).DataType = 'double';
+elems(7).Complexity = 'real';
+elems(7).Min = [];
+elems(7).Max = [];
+elems(7).DocUnits = '';
+elems(7).Description = '';
+
+elems(8) = Simulink.BusElement;
+elems(8).Name = 'CmdStart';
+elems(8).Dimensions = 1;
+elems(8).DimensionsMode = 'Fixed';
+elems(8).DataType = 'double';
+elems(8).Complexity = 'real';
+elems(8).Min = [];
+elems(8).Max = [];
+elems(8).DocUnits = '';
+elems(8).Description = '';
+
+elems(9) = Simulink.BusElement;
+elems(9).Name = 'CmdZero';
+elems(9).Dimensions = 1;
+elems(9).DimensionsMode = 'Fixed';
+elems(9).DataType = 'double';
+elems(9).Complexity = 'real';
+elems(9).Min = [];
+elems(9).Max = [];
+elems(9).DocUnits = '';
+elems(9).Description = '';
+
+elems(10) = Simulink.BusElement;
+elems(10).Name = 'CmdAir';
+elems(10).Dimensions = 1;
+elems(10).DimensionsMode = 'Fixed';
+elems(10).DataType = 'double';
+elems(10).Complexity = 'real';
+elems(10).Min = [];
+elems(10).Max = [];
+elems(10).DocUnits = '';
+elems(10).Description = '';
+
+elems(11) = Simulink.BusElement;
+elems(11).Name = 'CmdRsrv1';
+elems(11).Dimensions = 1;
+elems(11).DimensionsMode = 'Fixed';
+elems(11).DataType = 'double';
+elems(11).Complexity = 'real';
+elems(11).Min = [];
+elems(11).Max = [];
+elems(11).DocUnits = '';
+elems(11).Description = '';
+
+elems(12) = Simulink.BusElement;
+elems(12).Name = 'CmdRsrv2';
+elems(12).Dimensions = 1;
+elems(12).DimensionsMode = 'Fixed';
+elems(12).DataType = 'double';
+elems(12).Complexity = 'real';
+elems(12).Min = [];
+elems(12).Max = [];
+elems(12).DocUnits = '';
+elems(12).Description = '';
+
+elems(13) = Simulink.BusElement;
+elems(13).Name = 'CmdRsrv3';
+elems(13).Dimensions = 1;
+elems(13).DimensionsMode = 'Fixed';
+elems(13).DataType = 'double';
+elems(13).Complexity = 'real';
+elems(13).Min = [];
+elems(13).Max = [];
+elems(13).DocUnits = '';
+elems(13).Description = '';
+
+elems(14) = Simulink.BusElement;
+elems(14).Name = 'CmdRsrv4';
+elems(14).Dimensions = 1;
+elems(14).DimensionsMode = 'Fixed';
+elems(14).DataType = 'double';
+elems(14).Complexity = 'real';
+elems(14).Min = [];
+elems(14).Max = [];
+elems(14).DocUnits = '';
+elems(14).Description = '';
+
+FromAppBus_Type = Simulink.Bus;
+FromAppBus_Type.HeaderFile = '';
+FromAppBus_Type.Description = '';
+FromAppBus_Type.DataScope = 'Auto';
+FromAppBus_Type.Alignment = -1;
+FromAppBus_Type.PreserveElementDimensions = 0;
+FromAppBus_Type.Elements = elems;
+clear elems;
+assignin('base','FromAppBus_Type', FromAppBus_Type);
+
