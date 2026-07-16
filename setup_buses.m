90372a204b485054e5eb91ae34b25e688bbb1d04
Aleksei <lolver36@gmail.com>
2026-07-16 10:32:03 +0300

    simulink/refactor: migrate ToApp interface to homogenous nonvirtual Bus Object Гомогенизация выходных данных модели: все 8 каналов (включая сырой сигнал s и толщину Hcapt) приведены к единому базовому типу double перед входом в Bus Creator. Демонтированы блоки Byte Pack и Byte Unpack. В приложении убрал парсинг typecast и индексацию вида 5001:5014 . Прием данных переведен на прямое обращение к свойствам структуры (data.s, data.Kzero, data.Hcapt)

--- setup_buses.m
+++ setup_buses.m
@@ -323,3 +323,103 @@ N_Bus_Type.Elements = elems;
 clear elems;
 assignin('base','N_Bus_Type', N_Bus_Type);
 
+% Bus object: ToAppBus_Type 
+clear elems;
+elems(1) = Simulink.BusElement;
+elems(1).Name = 's';
+elems(1).Dimensions = 5000;
+elems(1).DimensionsMode = 'Fixed';
+elems(1).DataType = 'single';
+elems(1).Complexity = 'real';
+elems(1).Min = [];
+elems(1).Max = [];
+elems(1).DocUnits = '';
+elems(1).Description = '';
+
+elems(2) = Simulink.BusElement;
+elems(2).Name = 'Nmean';
+elems(2).Dimensions = 1;
+elems(2).DimensionsMode = 'Fixed';
+elems(2).DataType = 'single';
+elems(2).Complexity = 'real';
+elems(2).Min = [];
+elems(2).Max = [];
+elems(2).DocUnits = '';
+elems(2).Description = '';
+
+elems(3) = Simulink.BusElement;
+elems(3).Name = 'N_GradAir';
+elems(3).Dimensions = 1;
+elems(3).DimensionsMode = 'Fixed';
+elems(3).DataType = 'single';
+elems(3).Complexity = 'real';
+elems(3).Min = [];
+elems(3).Max = [];
+elems(3).DocUnits = '';
+elems(3).Description = '';
+
+elems(4) = Simulink.BusElement;
+elems(4).Name = 'Kzero';
+elems(4).Dimensions = 1;
+elems(4).DimensionsMode = 'Fixed';
+elems(4).DataType = 'single';
+elems(4).Complexity = 'real';
+elems(4).Min = [];
+elems(4).Max = [];
+elems(4).DocUnits = '';
+elems(4).Description = '';
+
+elems(5) = Simulink.BusElement;
+elems(5).Name = 'ObjCaptFlag';
+elems(5).Dimensions = 1;
+elems(5).DimensionsMode = 'Fixed';
+elems(5).DataType = 'single';
+elems(5).Complexity = 'real';
+elems(5).Min = [];
+elems(5).Max = [];
+elems(5).DocUnits = '';
+elems(5).Description = '';
+
+elems(6) = Simulink.BusElement;
+elems(6).Name = 'ObjCaptN_Corr';
+elems(6).Dimensions = 1;
+elems(6).DimensionsMode = 'Fixed';
+elems(6).DataType = 'single';
+elems(6).Complexity = 'real';
+elems(6).Min = [];
+elems(6).Max = [];
+elems(6).DocUnits = '';
+elems(6).Description = '';
+
+elems(7) = Simulink.BusElement;
+elems(7).Name = 'ChartState';
+elems(7).Dimensions = 1;
+elems(7).DimensionsMode = 'Fixed';
+elems(7).DataType = 'single';
+elems(7).Complexity = 'real';
+elems(7).Min = [];
+elems(7).Max = [];
+elems(7).DocUnits = '';
+elems(7).Description = '';
+
+elems(8) = Simulink.BusElement;
+elems(8).Name = 'Hcapt';
+elems(8).Dimensions = 1;
+elems(8).DimensionsMode = 'Fixed';
+elems(8).DataType = 'single';
+elems(8).Complexity = 'real';
+elems(8).Min = [];
+elems(8).Max = [];
+elems(8).DocUnits = '';
+elems(8).Description = '';
+
+ToAppBus_Type = Simulink.Bus;
+ToAppBus_Type.HeaderFile = '';
+ToAppBus_Type.Description = '';
+ToAppBus_Type.DataScope = 'Auto';
+ToAppBus_Type.Alignment = -1;
+ToAppBus_Type.PreserveElementDimensions = 0;
+ToAppBus_Type.Elements = elems;
+clear elems;
+assignin('base','ToAppBus_Type', ToAppBus_Type);
+
